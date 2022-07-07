918cffa927170092b16fd47330c16198c84783b6
zenocodes <azronebrian@gmail.com>
2022-07-07 11:49:18 +0300

    added login & signup functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mysql from 'mysql'
+import bcrypt from 'bcrypt'
 
 const app = express()
 const connection = mysql.createConnection({
@@ -18,13 +19,100 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+// login 
 app.get('/login', (req, res) => {
-    res.render('login')
+    const user = {
+        email: '', 
+        password: ''
+    }
+    res.render('login', {error: false, user: user})
 })
 
-app.get('/signup', (req, res) => {
-    res.render('signup')
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    let sql = 'SELECT * FROM job_seekers WHERE email = ?'
+    connection.query(
+        sql, [user.email], (error, results) => {
+            if (results.length > 0) {
+
+                bcrypt.compare(user.password, results[0].password, (error, matches) => {
+                    if (matches) {
+                        res.send('logged in successfully')                        
+                    } else {
+                        let message = 'Incorrect password'
+                        res.render('login', {error: true, message: message, user: user})
+                    }
+                })
+                
+            } else {
+                let message = 'Account does not exist. Please create one.'
+                res.render('login', {error: true, message: message, user: user})                
+            }
+        }
+    )
+
 })
+
+// sign up
+app.get('/signup', (req, res) => {
+    const user = {
+        fullname: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+    res.render('signup', {error: false, user: user})
+})
+
+app.post('/signup', (req, res) => {
+    const user = {
+        fullname: req.body.fullname,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    }
+
+    if (user.password === user.confirmPassword) {
+
+        let sql = 'SELECT * FROM job_seekers WHERE email = ?'
+        connection.query(
+            sql, [user.email], (error, results) => {
+                if (results.length > 0) {
+                    let message = 'Account already exists with the email provided.'
+                    res.render('signup', {error: true, message: message, user: user})
+                } else {
+
+                    bcrypt.hash(user.password, 10, (error, hash) => {
+                        let sql = 'INSERT INTO job_seekers (fullname, email, password) VALUES (?,?,?)'
+                        connection.query(
+                            sql, 
+                            [
+                                user.fullname,
+                                user.email,
+                                hash
+                            ],
+                            (error, results) => {
+                                res.redirect('/login')
+                            }
+                        )
+                    })
+                    
+                }
+            }
+        )
+        
+    } else {
+        let message = 'Passwords not matching.'
+        res.render('signup', {error: true, message: message, user: user})
+    }
+
+
+})
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
